669b89abc8c0b763ccd5646aded86f1635a6f84e
Sebastián Fornelli <cf18sebastian.fornelli@iesjoandaustria.org>
2019-05-19 19:02:37 +0200

    afegit importTotal()

--- src/Client.java
+++ src/Client.java
@@ -39,27 +39,35 @@ public class Client {
     }
 
     public String informe() {
-        double total = 0;
         int bonificacions = 0;
         String resultat = "Informe de lloguers del client " +
                 getNom() +
                 " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = lloguer.quantitat();
             bonificacions += lloguer.bonificacions();
             // composa els resultats d'aquest lloguer
             resultat += "\t" +
                     lloguer.getVehicle().getMarca() +
                     " " +
                     lloguer.getVehicle().getModel() + ": " +
-                    (quantitat * 30) + "€" + "\n";
-            total += quantitat * 30;
+                    (lloguer.quantitat() * 30) + "€" + "\n";
         }
 
         // afegeix informació final
-        resultat += "Import a pagar: " + total + "€\n" +
+        resultat += "Import a pagar: " + importTotal() + "€\n" +
                 "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+
+    private double importTotal() {
+
+        double total = 0;
+
+        for(Lloguer lloguer: lloguers) {
+            total += lloguer.quantitat() * 30;
+        }
+
+        return total;
     }
 
 
